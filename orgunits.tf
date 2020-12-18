727392d095cbd85cad407ee2027d81748693a35c
Christopher Dearie <cdearie@gmail.com>
2020-12-18 02:06:39 +0000

    fixed resource

--- orgunits.tf
+++ orgunits.tf
@@ -3,20 +3,20 @@ data "aws_organizations_organization" "my_org" {}
 
 resource "aws_organizations_organizational_unit" "core" {
   name      = "core"
-  parent_id = aws_organizations_organization.my_org.roots[0].id
+  parent_id = data.aws_organizations_organization.my_org.roots[0].id
 }
 
 resource "aws_organizations_organizational_unit" "teams" {
   name      = "teams"
-  parent_id = aws_organizations_organization.my_org.roots[0].id
+  parent_id = data.aws_organizations_organization.my_org.roots[0].id
 }
 
 resource "aws_organizations_organizational_unit" "network" {
   name      = "network"
-  parent_id = aws_organizations_organization.my_org.roots[0].id
+  parent_id = data.aws_organizations_organization.my_org.roots[0].id
 }
 
 resource "aws_organizations_organizational_unit" "administration" {
   name      = "administration"
-  parent_id = aws_organizations_organization.my_org.roots[0].id
+  parent_id = data.aws_organizations_organization.my_org.roots[0].id
 }
